b148f8d0286407f1351e0b8ad31d82915dea18e0
Antoine Adam <bidetzz@TonyPC>
2024-09-19 21:59:30 -0400

    terraform fmt for my eyes

--- local.tf
+++ local.tf
@@ -1,13 +1,13 @@
 locals {
-    ubuntu_ami_name_latest = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"
-    canonical_account_id =  "099720109477"
+  ubuntu_ami_name_latest = "ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"
+  canonical_account_id   = "099720109477"
 
-    aws_instances = {
-        blue = {
-            ssh_reachable = false
-        },
-        orange = {
-            ssh_reachable = true
-        }
+  aws_instances = {
+    blue = {
+      ssh_reachable = false
+    },
+    orange = {
+      ssh_reachable = true
     }
+  }
 }
--- main.tf
+++ main.tf
@@ -19,33 +19,33 @@ data "aws_ami" "ubuntu" {
 # Security groups
 
 resource "aws_security_group" "blue_sg" {
-  name = "blue_sg"
-  vpc_id = var.vpc_id
+  name        = "blue_sg"
+  vpc_id      = var.vpc_id
   description = "Security Group to allow outbound SSH to the orange instance from the blue instance"
 }
 
 resource "aws_vpc_security_group_egress_rule" "blue-outbound-ssh" {
-    security_group_id = aws_security_group.blue_sg.id
-    description = "Allow outbound SSH traffic from blue EC2 instance to orange EC2 instance"
-    referenced_security_group_id = aws_security_group.orange_sg.id
-    from_port = 22
-    to_port = 22
-    ip_protocol = "tcp"
+  security_group_id            = aws_security_group.blue_sg.id
+  description                  = "Allow outbound SSH traffic from blue EC2 instance to orange EC2 instance"
+  referenced_security_group_id = aws_security_group.orange_sg.id
+  from_port                    = 22
+  to_port                      = 22
+  ip_protocol                  = "tcp"
 }
 
 resource "aws_security_group" "orange_sg" {
-  name = "orange_sg"
-  vpc_id = var.vpc_id
+  name        = "orange_sg"
+  vpc_id      = var.vpc_id
   description = "Security Group to allow inbound SSH from the blue instance to the orange instance"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "orange-inbound-ssh" {
-    security_group_id = aws_security_group.orange_sg.id
-    description = "Allow inbound SSH traffic from blue EC2 instance to orange EC2 instance"
-    referenced_security_group_id = aws_security_group.blue_sg.id
-    from_port = 22
-    to_port = 22
-    ip_protocol = "tcp"
+  security_group_id            = aws_security_group.orange_sg.id
+  description                  = "Allow inbound SSH traffic from blue EC2 instance to orange EC2 instance"
+  referenced_security_group_id = aws_security_group.blue_sg.id
+  from_port                    = 22
+  to_port                      = 22
+  ip_protocol                  = "tcp"
 }
 
 # SSH key pair
@@ -57,13 +57,13 @@ resource "aws_key_pair" "fun_key" {
 
 # EC2
 resource "aws_instance" "ec2_instance" {
-  for_each = local.aws_instances  
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.ec2_instance_type
+  for_each                    = local.aws_instances
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.ec2_instance_type
   associate_public_ip_address = false
-  subnet_id = var.subnet_id
-  vpc_security_group_ids = each.value.ssh_reachable ? [aws_security_group.orange_sg.id] : [aws_security_group.blue_sg.id]
-  key_name = aws_key_pair.fun_key.key_name
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = each.value.ssh_reachable ? [aws_security_group.orange_sg.id] : [aws_security_group.blue_sg.id]
+  key_name                    = aws_key_pair.fun_key.key_name
   tags = {
     Name = each.key
   }
